c3e67f8705c114893e7312fdc4a394c1d6ba928e
Andersonpolanco1 <andersonpolancocontreras@gmail.com>
2023-10-05 20:59:35 -0400

    Adding ServiceCollectionExtension class

---
+++ Auth/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Auth.Models.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Auth.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddCustomAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection(JwtSettings.SettingPath).Get<JwtSettings>();
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = jwtSettings.ValidateIssuer,
+                    ValidateAudience = jwtSettings.ValidateAudience,
+                    ValidateLifetime = jwtSettings.ValidateLifetime,
+                    ValidateIssuerSigningKey = jwtSettings.ValidateIssuerSigningKey,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Issuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key)),
+                    ClockSkew = jwtSettings.ClockSkew
+                };
+            });
+        }
+    }
+}
