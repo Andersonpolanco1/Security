545788c75551adeaab6ffc21b4bc56248cebacdb
Andersonpolanco1 <andersonpolancocontreras@gmail.com>
2023-10-05 22:37:06 -0400

    Adding context

---
+++ Auth/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Auth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
